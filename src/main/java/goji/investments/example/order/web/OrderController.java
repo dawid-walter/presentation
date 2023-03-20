68609560298e4116d602369a27fd55777dd4d119
davedohc <davedohc@aim.com>
2023-03-20 10:02:33 +0000

    Add 3 methods for filtered, paginated and sorted results in order controller for REST API

--- src/main/java/goji/investments/example/order/web/OrderController.java
+++ src/main/java/goji/investments/example/order/web/OrderController.java
@@ -1,8 +1,10 @@
 package goji.investments.example.order.web;
 
 import goji.investments.example.order.OrderCRUDService;
+import goji.investments.example.order.model.CurrencyType;
 import goji.investments.example.order.model.Order;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,5 +37,22 @@ public class OrderController {
     public String create(@RequestBody CreateOrderRequest request) {
         Order add = orderService.add(request);
         return add.getId().toString();
+    }
+
+    @GetMapping("/filtered")
+    public List<Order> getFiltered(@RequestParam CurrencyType currencyType) {
+        return orderService.filterByCurrency(currencyType);
+    }
+
+    @GetMapping("/paginated")
+    public List<Order> getPaginated(@RequestParam(required = false) Integer page,
+                                    @RequestParam(required = false) Integer size) {
+        return orderService.findAllPaged(page, size);
+    }
+
+    @GetMapping("/sorted")
+    public List<Order> getSorted(@RequestParam(required = false) String sortBy,
+                                 @RequestParam(required = false) Sort.Direction sort) {
+        return orderService.findAllSorted(sortBy, sort);
     }
 }
