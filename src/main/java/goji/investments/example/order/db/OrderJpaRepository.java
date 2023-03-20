50182338c44fe612fb684f466db4215e0526f43e
davedohc <davedohc@aim.com>
2023-03-20 08:51:55 +0000

    Add interface for order repository

---
+++ src/main/java/goji/investments/example/order/db/OrderJpaRepository.java
@@ -0,0 +1,9 @@
+package goji.investments.example.order.db;
+
+import goji.investments.example.order.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+interface OrderJpaRepository extends JpaRepository<Order, UUID> {
+}
